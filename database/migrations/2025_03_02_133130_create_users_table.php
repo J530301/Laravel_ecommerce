52fa4938eb139915422fee28d5fcc3924aa27a9e
Jefferson Adlawan <J530301>
2025-05-16 11:39:18 +0800

    update table

--- database/migrations/2025_03_02_133130_create_users_table.php
+++ database/migrations/2025_03_02_133130_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
+            $table->string('email')->unique();
             $table->string('password');
             $table->boolean('is_active')->default(true); // Add a column for account status if needed
             $table->timestamps();
